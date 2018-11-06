f27224057e17064df019d64553d1121ec74a2352
Mario Retana <maretanar@gmail.com>
2018-11-06 17:35:23 -0600

    props lesson

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import Bananas from './components/Bananas';
+import LotsOfGreetings from './components/LotsOfGreetings'
 
-export default class App extends React.Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <>
+        <Bananas />
+        <LotsOfGreetings />
+      </>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
