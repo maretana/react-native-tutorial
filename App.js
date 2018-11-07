1a9c8537605b335471248abdf4dccf5288e76ee8
Mario Retana <maretanar@gmail.com>
2018-11-06 18:06:01 -0600

    Height and Width lesson

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
 import Bananas from './components/Bananas';
 import LotsOfGreetings from './components/LotsOfGreetings';
 import BlinkApp from './components/BlinkApp';
 import LotsOfStyles from './components/LotsofStyles';
+import FixedDimensionsBasics from './components/FixedDimensionsBasics';
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <>
+      <ScrollView>
         <Bananas />
         <LotsOfGreetings />
         {/* <BlinkApp /> <-- annoying*/}
         <LotsOfStyles />
-      </>
+        <FixedDimensionsBasics />
+      </ScrollView>
     );
   }
 }
---
+++ components/FixedDimensionsBasics.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class FixedDimensionsBasics extends Component {
+  render() {
+    return (
+      <View style={{width: '100%', height: 300}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+}
